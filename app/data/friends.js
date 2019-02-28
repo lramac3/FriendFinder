42671f6ff5a1f5834cf1733e21776c2f34eab493
Linu Ramachandran <linuram@gmail.com>
2019-02-28 02:14:12 -0500

    updated friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+let friendArray = [
+    {
+        "name": "Tom",
+        "photo": "https://vignette.wikia.nocookie.net/characters/images/a/a9/Tj_tom_174x52.png/revision/latest?cb=20130429031506",
+        "scores": ["5","5","5","5","5","5","5","5","5","5"]
+    },
+    {
+        "name": "Thomas",
+        "photo": "https://images-na.ssl-images-amazon.com/images/I/71ls03uttFL._SX466_.jpg",
+        "scores": ["1","1","1","1","1","1","1","1","1","1"]
+    },
+     {
+        "name": "Velma",
+        "photo": "http://foreveryoungadult.com/_uploads/images2/velma.jpg",
+        "scores": ["3","3","3","3","3","3","3","3","3","3"]
+    },
+     {
+        "name": "Moana",
+        "photo": "https://imgc.allpostersimages.com/img/print/posters/moana_a-G-14531171-0.jpg",
+        "scores": ["4","4","4","4","4","4","4","4","4","4"]
+    },
+    {
+        "name": "Jerry",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxVieF_bxUFjLRrdIrkhkrChAJLRW6HYpyCu2Mh2OEk5q2omoz",
+        "scores": ["2","2","2","2","2","2","2","2","2","2"]
+    }
+
+]
+
+module.exports = friendArray;
