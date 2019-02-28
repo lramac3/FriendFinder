ee27e13e72580512393f8be3c3067ecc223e5593
Linu Ramachandran <linuram@gmail.com>
2019-02-28 01:28:21 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
+//Dependencies
 let express = require("express");
 let bodyParser = require("body-parser");
 let path = require("path");
 let app = express();
 let PORT = process.env.PORT || 8080;
-
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 app.use(express.static("app/public"));
-
+//app.use(express.static(path.join(__dirname, '/app/public')));
+// Route requiring
 require("./app/public/routing/apiRoutes.js")(app);
 require("./app/public/routing/htmlRoutes.js")(app);
-
+//Listener
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
 });
